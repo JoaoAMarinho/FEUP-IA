03cb6c374239da9c07440f8cb43e71c620191b32
Margarida Vieira <margaridajcv.27@gmail.com>
2022-04-24 10:11:59 +0100

    plot of the comparison of the different methods

--- src/tests/test_algorithms.py
+++ src/tests/test_algorithms.py
@@ -1,5 +1,7 @@
 from config_import import *
 
+from algorithms.genetic.Selection import RoulleteSelection
+from algorithms.genetic.Crossover import PoolsCrossover
 
 class TestAlgorithms(unittest.TestCase):
     @classmethod
@@ -13,25 +15,27 @@ class TestAlgorithms(unittest.TestCase):
         print('got solution')
     
     def test_simulated_annealing(self):
-        simulated_annealing = SimulatedAnnealing(self.solution)
-        solution = simulated_annealing.execute(self.callback,f'simulated_annealing3.json')
+        simulated_annealing = SimulatedAnnealing(initial_solution=self.solution, 
+                                                 initial_temperature=1000000, 
+                                                 temperature_decrease_factor=0.75)
+        solution = simulated_annealing.execute(self.callback,f'simulated_annealing_best.json')
         print("Sim\n",solution, "Eval: ",solution.evaluation, " Time: ",solution.time)
 
     def test_genetic(self):
-        genetics = GeneticAlgorithm(self.solution)
-        solution = genetics.execute(self.callback, f'genetic3.json')
+        genetics = GeneticAlgorithm(initial_solution=self.solution,
+                                    selection_method=RoulleteSelection(),
+                                    crossover_method=PoolsCrossover())
+        solution = genetics.execute(self.callback, f'genetic_best.json')
         print("Tabu\n", solution, "Eval ", solution.evaluation)
 
     def test_hill_climbing(self):
-        solution = self.data_center.initial_solution()
-        hill_climbing = HillClimbing(solution)
-        solution = hill_climbing.execute(
-            self.callback, f'hill_climbing3.json')
+        hill_climbing = HillClimbing(initial_solution=self.solution)
+        solution = hill_climbing.execute(self.callback, f'hill_climbing_best.json')
         print("Hill\n", solution, "Eval ", solution.evaluation)
 
     def test_tabu_search(self):
-        tabu_search = TabuSearch(self.solution)
-        solution = tabu_search.execute(self.callback,f'tabu_search3.json')
+        tabu_search = TabuSearch(initial_solution=self.solution)
+        solution = tabu_search.execute(self.callback,f'tabu_search_best.json')
         print("Tabu\n", solution, "Eval ", solution.evaluation)
 
 if __name__ == '__main__':
