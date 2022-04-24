016e6d8ef522a77d6f6d42b9dd3032cfe427dd28
JoaoMarinho <61231873+JoaoAMarinho@users.noreply.github.com>
2022-04-24 17:07:11 +0100

    tabu search update

--- src/algorithms/search/TabuSearch.py
+++ src/algorithms/search/TabuSearch.py
@@ -14,7 +14,6 @@ class TabuSearch(Algorithm):
         Returns:
             The best solution found
         """
-
 		start = perf_counter()
 
 		iteration = 0
@@ -22,27 +21,31 @@ class TabuSearch(Algorithm):
 
 		solution = self.initial_solution
 		best_solution = solution.evaluation
-		
+
 		while not self.stop(iteration, iteration_no_imp):
 			iteration += 1
 			iteration_no_imp += 1
 
 			neighbours = self.neighbour_solutions(solution)
-			new_solution = self.get_best_solution(neighbours)
+			new_solution = neighbours[0]
+
+			for neighbour in neighbours:
+				key = hash(neighbour)
+				if (key not in self.tabu_memory):
+					if neighbour.evaluation > new_solution.evaluation:
+						new_solution = neighbour
+				else:
+					tenure = self.tabu_memory[key].tenure
+					if tenure == 0: self.tabu_memory.pop(key)
+					else: self.tabu_memory[key].tenure = tenure - 1
+
+			if new_solution.evaluation > best_solution:
+				solution = new_solution
+				best_solution = new_solution.evaluation
 
 			key = hash(new_solution)
-			if key not in self.tabu_memory:
-				if new_solution.evaluation > best_solution:
-					iteration_no_imp = 0
-					solution = new_solution
-					best_solution = new_solution.evaluation
-					self.tabu_memory[key] = solution
-					self.tabu_memory[key].tenure = self.tabu_tenure
-			else:
-				tenure = self.tabu_memory[key].tenure
-				if tenure == 0: self.tabu_memory.pop(key)
-				else: self.tabu_memory[key].tenure = tenure - 1
-
+			self.tabu_memory[key] = new_solution
+			self.tabu_memory[key].tenure = self.tabu_tenure
 
 		elapsed = perf_counter() - start
 		solution.time = elapsed
